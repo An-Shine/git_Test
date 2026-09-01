9ec98ba9300e8d1bcc219457053deaf5053e17a2
An-Shine <akh1012@naver.com>
2026-09-01 15:19:16 +0900

    initial commit
    초기 커밋

---
+++ Assets/Scripts/Day5/Git_Test.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Git_Test : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // 테스트 테스트
+    }
+
+    private void Update()
+    {
+        // 테스트 테스트
+    }
+}
